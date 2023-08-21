6a414b0674d38b2d5db3018b37ae00f6eefd8608
AlexSereb666 <aserebrakov343@gmail.com>
2023-08-21 20:02:52 +0300

    123

--- client/src/components/pages/coffeeHouseCard/CoffeeHouseCard.jsx
+++ client/src/components/pages/coffeeHouseCard/CoffeeHouseCard.jsx
@@ -1,11 +1,84 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
+import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import './CoffeeHouseCard.css';
 
 function CoffeeHouseCard() {
     let { id } = useParams();
 
+    let coffeeHouses = [{
+        id         : 1,
+        name       : 'Coffee House 1',
+        phone      : '',
+        address    : '',
+        score      : 10,
+        orders     : 0,
+        coordinates: [55.75, 37.57],
+        schedule   : '',
+        description: '',
+        gallery    : []
+    },
+    {
+        id         : 2,
+        name       : 'Coffee House 2',
+        phone      : '',
+        address    : '',
+        score      : 10,
+        orders     : 0,
+        coordinates: [55.75, 37.57],
+        schedule   : '',
+        description: '',
+        gallery    : []
+    },
+    {
+        id         : 3,
+        name       : 'Coffee House 3',
+        phone      : '',
+        address    : '',
+        score      : 10,
+        orders     : 0,
+        coordinates: [55.75, 37.57],
+        schedule   : '',
+        description: '',
+        gallery    : []
+    },
+    {
+        id         : 4,
+        name       : 'Coffee House 4',
+        phone      : '',
+        address    : '',
+        score      : 10,
+        orders     : 0,
+        coordinates: [55.75, 37.57],
+        schedule   : '',
+        description: '',
+        gallery    : []
+    }]
+
+    const mapData = {
+        center: [55.75, 37.57],
+        zoom: 10,
+    };
+
+    const placemarkGeometry = [55.75, 37.57];
+
+    const selectedCoffeeHouse = coffeeHouses.find(coffeeHouse => coffeeHouse.id === parseInt(id));
+
     return (
-        <div>
-            <h1>Detail of Coffee House with ID: {id}</h1>
+        <div className="coffee-house-card">
+            <div className="coffee-house-card__left">
+                <h1 className='coffee-house-card__title'>{selectedCoffeeHouse.name}</h1>
+                <p>Some text about the coffee house...</p>
+            </div>
+            <div className="coffee-house-card__right">
+                <div className="coffee-house-card__right__map-container">
+                    <YMaps>
+                        <Map defaultState={mapData} width='100%' height='100%'>
+                            <Placemark geometry={placemarkGeometry} />
+                        </Map>
+                    </YMaps>
+                </div>
+            </div>
         </div>
     );
 }
